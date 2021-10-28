47fd7af206091e51fc9e2fd55dae2902affaec74
ben <benedikt.maciejczyk@gmx.de>
2021-10-28 16:40:19 +0200

    add axios to frontend

--- backend/src/test/java/capstone/backend/BackendApplicationTests.java
+++ backend/src/test/java/capstone/backend/BackendApplicationTests.java
@@ -1,13 +1,16 @@
-//package capstone.backend;
-//
-//import org.junit.jupiter.api.Test;
-//import org.springframework.boot.test.context.SpringBootTest;
-//
-//@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-//class BackendApplicationTests {
-//
-//    @Test
-//    void contextLoads() {
-//    }
-//
-//}
+package capstone.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
+@SpringBootTest()
+class BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
