37a313419368a4bacc0e7dbe3bc013c226d02456
ben <benedikt.maciejczyk@gmx.de>
2021-10-29 15:53:40 +0200

    install react-router and wrap app inrouter

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -5,17 +5,20 @@ import App from './App';
 import { store } from './app/store';
 import { Provider } from 'react-redux';
 import * as serviceWorker from './serviceWorker';
+import history from './services/history';
+import {Router } from "react-router-dom";
+
 
 ReactDOM.render(
   <React.StrictMode>
+      <Router history={history}>
     <Provider store={store}>
       <App />
     </Provider>
+      </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+
 serviceWorker.unregister();
