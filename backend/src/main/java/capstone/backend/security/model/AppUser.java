9897c44099a76bb5f10ab7d5af7f11402571bc79
ben <benedikt.maciejczyk@gmx.de>
2021-11-01 20:33:11 +0100

    update security package

---
+++ backend/src/main/java/capstone/backend/security/model/AppUser.java
@@ -0,0 +1,41 @@
+package capstone.backend.security.model;
+
+import capstone.backend.models.db.contact.Employee;
+import lombok.*;
+import org.hibernate.Hibernate;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+import java.util.Objects;
+
+@Getter
+@Setter
+@ToString
+@RequiredArgsConstructor
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+public class AppUser {
+
+    @Id
+    private String username;
+    private String password;
+    @OneToOne
+    private Employee employee;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
+        AppUser appUser = (AppUser) o;
+        return username != null && Objects.equals(username, appUser.username);
+    }
+
+    @Override
+    public int hashCode() {
+        return getClass().hashCode();
+    }
+
+}
---
+++ backend/src/main/java/capstone/backend/security/model/UserDTO.java
@@ -0,0 +1,19 @@
+package capstone.backend.security.model;
+
+import capstone.backend.models.db.contact.Employee;
+import capstone.backend.models.dto.contact.EmployeeDTO;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class UserDTO {
+
+    private String username;
+    private String password;
+    private EmployeeDTO employee;
+}
