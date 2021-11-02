6322e1628a7371db79da9534f184781e80b9744d
ben <benedikt.maciejczyk@gmx.de>
2021-11-02 14:48:32 +0100

    move jwt functions in their own service

--- frontend/src/services/authService.ts
+++ frontend/src/services/authService.ts
@@ -2,21 +2,6 @@ import axios from "axios";
 import {ICredentials} from "../interfaces/IEmployee";
 import {parseError} from './errorService';
 
-
-const parseJwt = (token: string) => {
-    try {
-        return JSON.parse(atob(token.split(".")[1]));
-    } catch (e) {
-        return null;
-    }
-}
-
-export const validateToken = (token: string): boolean => {
-    const decodedJwt = parseJwt(token);
-    if (!decodedJwt) return false;
-    return decodedJwt.exp * 1000 > Date.now()
-}
-
 export const sendLoginData = (credentials: ICredentials) => {
     return axios({
         method: 'post',
