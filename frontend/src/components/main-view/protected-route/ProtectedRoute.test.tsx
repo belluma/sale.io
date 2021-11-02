d9f3cb40c867c1ca9b5232acb303a5e95c9128e3
ben <benedikt.maciejczyk@gmx.de>
2021-11-02 15:12:13 +0100

    add protected route

---
+++ frontend/src/components/main-view/protected-route/ProtectedRoute.test.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import {render, screen} from '@testing-library/react';import ProtectedRoute from './ProtectedRoute';
+import {store} from "../../../app/store";
+import {Provider} from "react-redux";
+
+
+it('renders without crashing', () => {
+    render(<Provider store={store}><ProtectedRoute /></Provider>);    });
