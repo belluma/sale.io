a7bf6b519bb6b686550f21bd6c6038c12d975c78
ben <benedikt.maciejczyk@gmx.de>
2021-11-02 11:17:49 +0100

    strip app to bare neccessities

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -5,17 +5,13 @@ import App from './App';
 import { store } from './app/store';
 import { Provider } from 'react-redux';
 import * as serviceWorker from './serviceWorker';
-import history from './services/history';
-import {Router } from "react-router-dom";
 
 
 ReactDOM.render(
   <React.StrictMode>
-      <Router history={history}>
     <Provider store={store}>
       <App />
     </Provider>
-      </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
