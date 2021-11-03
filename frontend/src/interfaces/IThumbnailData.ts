05697fcd99cb684f7ec988e0e6ce79fdf78055d8
ben <benedikt.maciejczyk@gmx.de>
2021-11-03 14:45:52 +0100

    create interface for thumbnail and helper function to parse data

---
+++ frontend/src/interfaces/IThumbnailData.ts
@@ -0,0 +1,14 @@
+import {IEmployee} from "./IEmployee";
+
+export interface IThumbnailData {
+    title:string,
+    subtitle?:string,
+    picture: string | undefined,
+}
+
+export const parseEmployeeToThumbnailData = (employee:IEmployee):IThumbnailData{
+    return {
+        title: `${employee.firstName} ${employee.lastName}`,
+        picture: employee.picture,
+    }
+}
