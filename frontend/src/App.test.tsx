3bdfe7512880dd912208508f876a5551aaad8758
ben <benedikt.maciejczyk@gmx.de>
2021-11-03 20:32:06 +0100

    sooze sonarlint

---
+++ frontend/src/App.test.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import { Provider } from 'react-redux';
+import { store } from './app/store';
+import App from './App';
+import {BrowserRouter as Router} from 'react-router-dom'
+
+test('renders component', () => {
+    const { getByText } = render(
+        <Provider store={store}>
+            <Router>
+                <App />
+            </Router>
+        </Provider>
+    );
+
+});
