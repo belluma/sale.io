c3bf3161378153ba2123ebc116f281468f9c3143
ben <benedikt.maciejczyk@gmx.de>
2021-11-04 09:21:40 +0100

    create basic routes in controller

--- backend/src/main/java/capstone/backend/controller/ProductController.java
+++ backend/src/main/java/capstone/backend/controller/ProductController.java
@@ -1,9 +1,38 @@
 package capstone.backend.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import capstone.backend.model.dto.ProductDTO;
+import capstone.backend.services.ProductService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("api/product")
 public class ProductController {
+
+    private final ProductService service;
+
+    public ProductController(ProductService productService) {
+        this.service = productService;
+    }
+
+    @GetMapping
+    public List<ProductDTO> getAllProductsWithDetails() {
+        return service.getAllProductsWithDetails();
+    }
+
+    @GetMapping("{id}")
+    public ProductDTO getProductDetails(@PathVariable Long id) {
+        return service.getProductDetails(id);
+    }
+
+    @PostMapping
+    public ProductDTO createProduct(@RequestBody ProductDTO product) {
+        return service.createProduct(product);
+    }
+    @PutMapping("{id}")
+    public ProductDTO editProduct(@PathVariable Long id, @RequestBody ProductDTO product) {
+        return service.editProduct(product);
+    }
+
 }
