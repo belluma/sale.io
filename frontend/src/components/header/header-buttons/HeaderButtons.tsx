e64a246b7f6717fe1d6282689cb2123654f5928d
ben <benedikt.maciejczyk@gmx.de>
2021-11-04 17:44:44 +0100

    move buttons in their own component, make logout button appear responsively

--- frontend/src/components/header/header-buttons/HeaderButtons.tsx
+++ frontend/src/components/header/header-buttons/HeaderButtons.tsx
@@ -1,14 +1,26 @@
 import React from 'react'
 
-//component imports
+import {useAppDispatch, useAppSelector} from "../../../app/hooks";
+import {logout, selectLoggedIn} from "../../../slicer/authSlice";
 
+//component imports
+import {IconButton, Toolbar} from "@mui/material";
+import LogoutIcon from "@mui/icons-material/Logout";
 //interface imports
 
 type Props = {};
 
-function HeaderButtons(props: Props){
-    return(
-       <div>HeaderButtons</div>
+function HeaderButtons(props: Props) {
+    const dispatch = useAppDispatch();
+    const loggedIn = useAppSelector(selectLoggedIn);
+    const handleLogout = () => dispatch(logout());
+    return (
+        <Toolbar sx={{mb: 1, alignItems: "stretch", justifyContent: "space-between"}}>
+            {loggedIn && <IconButton onClick={handleLogout} edge="end">
+                <LogoutIcon/>
+            </IconButton>}
+        </Toolbar>
+
     )
 }
 
