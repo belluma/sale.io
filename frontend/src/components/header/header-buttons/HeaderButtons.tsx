b07698ff2ebe16194da001617a98a12c6557b1f3
ben <benedikt.maciejczyk@gmx.de>
2021-11-09 13:27:15 +0100

    add burger icon on mobile view

--- frontend/src/components/header/header-buttons/HeaderButtons.tsx
+++ frontend/src/components/header/header-buttons/HeaderButtons.tsx
@@ -4,8 +4,9 @@ import {useAppDispatch, useAppSelector} from "../../../app/hooks";
 import {logout, selectLoggedIn} from "../../../slicer/authSlice";
 
 //component imports
-import {Button, IconButton, Toolbar} from "@mui/material";
+import {Button, IconButton, Toolbar, useMediaQuery, useTheme} from "@mui/material";
 import LogoutIcon from "@mui/icons-material/Logout";
+import MenuIcon from "@mui/icons-material/Menu";
 //interface imports
 import {Views} from "../../../interfaces/IThumbnailData";
 import {useHistory} from "react-router";
@@ -14,16 +15,20 @@ import {views} from "../../main-view/helpers";
 type Props = {};
 
 function HeaderButtons(props: Props) {
+    const theme = useTheme();
+    const smallScreen = useMediaQuery(theme.breakpoints.down('sm'));
     const history = useHistory()
     //@ts-ignore
     const reroute = (e:React.MouseEvent<HTMLButtonElement>) => {history.push(Views[e.currentTarget.name])}
     const dispatch = useAppDispatch();
     const loggedIn = useAppSelector(selectLoggedIn);
     const handleLogout = () => dispatch(logout());
-    const buttons = views.map((view) => <Button key={view} name={view} onClick={reroute}>{view}S</Button>)
+    const buttons =views.map((view) => <Button key={view} name={view} onClick={reroute}>{view}S</Button>)
     return (
         <Toolbar sx={{mb: 1, alignItems: "stretch", justifyContent: "space-between"}}>
-            {buttons}
+            {smallScreen ? <IconButton>
+                <MenuIcon/>
+            </IconButton> :buttons}
             {loggedIn && <IconButton onClick={handleLogout} edge="end">
                 <LogoutIcon/>
             </IconButton>}
