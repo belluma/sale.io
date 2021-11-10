cc0dae516d5713b77b28fd897a14eba11e47782a
ben <benedikt.maciejczyk@gmx.de>
2021-11-10 17:56:05 +0100

    and one more

--- frontend/src/components/main-view/views/employees/Employees.tsx
+++ frontend/src/components/main-view/views/employees/Employees.tsx
@@ -1,6 +1,6 @@
 import React, {useEffect} from 'react'
 import {useAppDispatch, useAppSelector} from "../../../../app/hooks";
-import {getAllProducts, selectProducts} from "../../../../slicer/productSlice";
+import { selectProducts} from "../../../../slicer/productSlice";
 import {parseProductToThumbnailData, Views} from "../../../../interfaces/IThumbnailData";
 import {selectView} from "../../../../slicer/viewSlice";
 import GridView from "../../grid-view/GridView";
