b603d1b275b5f2db4165d3565706f907f53fd1df
ben <benedikt.maciejczyk@gmx.de>
2021-11-14 10:07:04 +0100

    rearange items on order card in grid

--- frontend/src/components/forms/order/Order.tsx
+++ frontend/src/components/forms/order/Order.tsx
@@ -60,27 +60,28 @@ function Order(props: Props) {
         setQuantity(0);
     }
     return (
-        <div>
-            <Toolbar>
+        <Grid container>
+            <Grid item xs={12}>
                 <CustomSelect label={'supplier'} value={selectedSupplierId} name={"supplier"} options={supplierOptions}
-                              onChange={selectSupplier} model="supplier" required disabled={orderToSave.items.length > 0}/>
-            </Toolbar>
-            <h2>Add items to your order</h2>
-            <Grid container>
-                <Grid item xs={8}>
-                    <CustomSelect label={'product'} value={selectedProductId} name="product" options={productOptions}
-                                  onChange={selectProduct}model="product" required disabled={!orderToSave.supplier}/>
-                </Grid>
-                <Grid item xs={2}>
-                    <CustomNumber label={'quantity'} value={quantity} name="quantity" onChange={changeQuantity}
-                                  model="order" required/>
-                </Grid>
-                <Grid item xs={2}>
-                    <Button disabled={!validateProduct} onClick={addProduct}>Add</Button>
-                </Grid>
+                              onChange={selectSupplier} model="supplier" required
+                              disabled={orderToSave.items.length > 0}/>
             </Grid>
-          <Preview />
-        </div>
+            <Grid item xs={12}>
+                <h2>Add items to your order</h2>
+            </Grid>
+            <Grid item xs={8}>
+                <CustomSelect label={'product'} value={selectedProductId} name="product" options={productOptions}
+                              onChange={selectProduct} model="product" required disabled={!orderToSave.supplier}/>
+            </Grid>
+            <Grid item xs={2}>
+                <CustomNumber label={'quantity'} value={quantity} name="quantity" onChange={changeQuantity}
+                              model="order" required/>
+            </Grid>
+            <Grid item xs={2}>
+                <Button disabled={!validateProduct} onClick={addProduct}>Add</Button>
+            </Grid>
+            <Preview/>
+        </Grid>
     )
 }
 
