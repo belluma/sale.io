dc88c85c2c5f18dacd7543127145a98beabb582f
ben <benedikt.maciejczyk@gmx.de>
2021-11-19 08:33:14 +0100

    add test for add items to order

--- backend/src/test/java/capstone/backend/services/OrderToCustomerServiceTest.java
+++ backend/src/test/java/capstone/backend/services/OrderToCustomerServiceTest.java
@@ -2,6 +2,7 @@ package capstone.backend.services;
 
 import capstone.backend.model.db.order.OrderToCustomer;
 import capstone.backend.model.db.order.OrderToSupplier;
+import capstone.backend.model.dto.order.OrderItemDTO;
 import capstone.backend.model.dto.order.OrderToCustomerDTO;
 import capstone.backend.model.enums.OrderStatus;
 import capstone.backend.repo.OrderToCustomerRepo;
@@ -10,15 +11,17 @@ import org.junit.runner.RunWith;
 import org.mockito.junit.MockitoJUnitRunner;
 
 import java.util.List;
+import java.util.Optional;
 
+import static capstone.backend.mapper.OrderItemMapper.mapOrderItem;
 import static capstone.backend.mapper.OrderToCustomerMapper.mapOrder;
 import static capstone.backend.model.enums.OrderStatus.OPEN;
+import static capstone.backend.utils.OrderItemTestUtils.sampleOrderItemDTO;
 import static capstone.backend.utils.OrderToCustomerTestUtils.*;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 @RunWith(MockitoJUnitRunner.class)
 class OrderToCustomerServiceTest {
@@ -64,6 +67,22 @@ class OrderToCustomerServiceTest {
 
     @Test
     void addItemsToOrder() {
+        //GIVEN
+        OrderToCustomerDTO expected = OrderDTOWithStatusOpenWithOrderItem();
+        OrderToCustomer orderWithItemAdded = orderOpenWithOrderItem();
+        OrderItemDTO itemToAdd = sampleOrderItemDTO();
+        when(orderRepo.findById(expected.getId())).thenReturn(Optional.of(emptyOrderOpen()));
+        when(productService.productExists(itemToAdd.getProduct())).thenReturn(true);
+        when(orderItemService.addItemsToExistingOrder(mapOrderItem(itemToAdd))).thenReturn(mapOrderItem(itemToAdd));
+        when(orderRepo.save(orderWithItemAdded)).thenReturn(orderWithItemAdded);
+        //WHEN
+        OrderToCustomerDTO actual = orderService.addItemsToOrder(expected.getId(), itemToAdd);
+        //THEN
+        verify(orderRepo, times(2)).findById(expected.getId());
+        verify(productService).productExists(itemToAdd.getProduct());
+        verify(orderItemService).addItemsToExistingOrder(mapOrderItem(itemToAdd));
+        verify(orderRepo).save(orderWithItemAdded);
+        assertThat(actual, is(expected));
     }
 
     @Test
