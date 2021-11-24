68e4f76e42f1913ea05c221c2292562970012444
ben <benedikt.maciejczyk@gmx.de>
2021-11-24 17:11:28 +0100

    disable buttons when loggedout

--- frontend/src/components/header/header-buttons/change-view/ChangeView.tsx
+++ frontend/src/components/header/header-buttons/change-view/ChangeView.tsx
@@ -6,13 +6,13 @@ import {selectView, toggleView} from "../../../../slicer/viewSlice";
 import { IconButton} from "@mui/material";
 import ListViewIcon from "@mui/icons-material/ViewList"
 import GridViewIcon from "@mui/icons-material/GridView"
-import {useButtonStyles} from "../styles";
+import {useIconButtonStyles} from "../styles";
 
 export default function ChangeView() {
 const dispatch = useAppDispatch()
     const gridView = useAppSelector(selectView);
 const handleClick = () => {dispatch(toggleView());}
-    const classes = useButtonStyles();
+    const classes = useIconButtonStyles();
     return (
             <IconButton classes={{root: classes.goldFont}}  type="button" onClick={handleClick}>
                 {gridView ? <GridViewIcon /> : <ListViewIcon />}
