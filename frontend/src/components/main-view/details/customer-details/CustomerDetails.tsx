17b6728e048343c46732cb95a76007b19e5dd076
ben <benedikt.maciejczyk@gmx.de>
2021-11-24 07:47:07 +0100

    close details view when forwarding to sales

--- frontend/src/components/main-view/details/customer-details/CustomerDetails.tsx
+++ frontend/src/components/main-view/details/customer-details/CustomerDetails.tsx
@@ -1,29 +1,32 @@
 import React from 'react'
 
+import {hideDetails} from "../../../../slicer/detailsSlice";
 import {selectCurrentCustomer} from "../../../../slicer/customerSlice";
-import {useAppSelector} from "../../../../app/hooks";
 
+import {useHistory} from "react-router";
+import {useAppDispatch, useAppSelector} from "../../../../app/hooks";
 //component imports
 import OrderDetails from "../order-details/OrderDetails";
+
 import {Button, CardHeader, Divider, Toolbar} from "@mui/material";
+
 import AttachMoneyIcon from '@mui/icons-material/AttachMoney'
-
 import AddIcon from '@mui/icons-material/Add'
-
 //interface imports
 import {IOrderItem} from "../../../../interfaces/IOrder";
-import {useHistory} from "react-router";
 
 type Props = {};
 
 function CustomerDetails(props: Props){
     const history = useHistory();
+    const dispatch = useAppDispatch();
     const currentCustomer = useAppSelector(selectCurrentCustomer);
     const orderItems: IOrderItem[] = [];
     const {id, name} = currentCustomer;
     const customerName = name ? name : `table ${id}`
     const openSalesView = () => {
         history.push('sales');
+        dispatch(hideDetails());
     }
     const Buttons = () => (
         <Toolbar>
