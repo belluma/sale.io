f8b15db8a19bb06fdf9fc9e0dc07911a8f4520c0
ben <benedikt.maciejczyk@gmx.de>
2021-12-04 06:16:32 +0100

    change select category function

--- frontend/src/components/forms/product/ProductForm.tsx
+++ frontend/src/components/forms/product/ProductForm.tsx
@@ -30,6 +30,10 @@ function ProductForm() {
         const selectedSupplier = suppliers.filter(s => s.id === e.target.value);
         dispatch(handleProductFormInput({...productToSave, suppliers: selectedSupplier}))
     }
+    const handleCategoryChange = (e: ChangeEvent<HTMLInputElement>) => {
+        const category = categories.find(c => c.id === +e.target.value)
+        dispatch(handleProductFormInput({...productToSave, category}))
+    }
     const props = {onChange: handleChange, model: "product"};
     const supplierOptions = mapSupplierToSelectData(suppliers);
     const categoryOptions = mapCategoryToSelectData(categories)
@@ -46,7 +50,7 @@ function ProductForm() {
                     </Grid>
                     <Grid item xs={6}>
                         <CustomSelect name="category" label={"category"} value={categoryId}
-                                      options={categoryOptions} onChange={handleChange} model={"category"} />
+                                      options={categoryOptions} onChange={handleCategoryChange} model={"category"} />
                     </Grid>
                     <Grid item xs={6}>
                         <CustomNumber currency name="purchasePrice" label={"purchase price"} value={purchasePrice} {...props} required/>
